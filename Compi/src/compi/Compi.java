929a4e6779702def3b1bd84e109a585be552b5a9
Alberto <https://github.com/SeatielArmenta/proyecto1.git>
2023-04-03 12:32:23 -0700

    matriz de palabras y error

--- Compi/src/compi/Compi.java
+++ Compi/src/compi/Compi.java
@@ -19,6 +19,35 @@ public class Compi {
         System.out.println("equisde");
         System.out.println("joto");
         System.out.println("ily");
+        
+        String palabrasReservadas[][] = {
+
+            {"break","200"},
+            {"if","201"},
+            {"else","202"},
+            {"main","203"},
+            {"while","204"},
+            {"goto","205"},
+            {"print","206"},
+            {"new","207"},
+            {"float","208"},
+            {"int","209"},
+            {"false","210"},
+            {"true","211"},
+            {"string","212"}
+
+        };
+
+        String errores[][]={
+
+            {"se espera digito","500"},
+            {"se espera cierre de comentario","501"},
+            {"se espera &","502"},
+            {"se espera un |","503"},
+            {"se espera cierre de cadena","504"},
+            {"caracter no valido","505"}
+
+        };
     }
     
 }
