e3ab9c1627f94b6143f33e5c451bcace90e6e4f9
seati <seati@DESKTOP-R29IEDD>
2023-05-13 23:11:44 -0700

    Seguimiento de la primer rama de statement, el IF.

--- Compi/src/compi/sintaxis.java
+++ Compi/src/compi/sintaxis.java
@@ -80,6 +80,7 @@ public class sintaxis {
 
             } else {
                 System.out.println("Se espera un identificador");
+
             }
 
         } else if (p.token == 125) {
@@ -92,6 +93,161 @@ public class sintaxis {
     }
 
     private void statements() {
+        if (p.token == 201) {
+            p = p.sig;
+            if (p.token == 117) {
+                p = p.sig;
+                exp_cond();
+                if (p.token==118) {
+                    p=p.sig;
+                    if (p.token==119) {
+                        p=p.sig;
+                        statements();
+                        if (p.token==120) {
+                            p=p.sig;
+                            if (p.token==202) {
+                                p=p.sig;
+                                if (p.token==119) {
+                                    p=p.sig;
+                                    statements();
+                                    if (p.token==120) {
+                                        p=p.sig;
+                                    }
+                                    else{
+                                        System.out.println("Se espera }");
+                                    }
+                                }
+                                else{
+                                    System.out.println("Se espera {");
+                                }
+                            }
+                        }
+                        else{
+                            System.out.println("Se espera }");
+                        }
+                    }
+                    else{
+                        System.out.println("Se espera {");
+                    }
+                    
+                }
+                else{
+                    System.out.println("Se espera )");
+                }
+            } else {
+                System.out.println("Se espera un (");
+            }
+        } else if (p.token == 204) {
+
+        } else if (p.token == 206) {
+
+        } else if (p.token == 100) {
+
+        } else {
+            System.out.println("Se espera un statement");
+        }
 
     }
+
+    private void exp_cond() {
+        exp_sim();
+        exp_rel();
+        exp_sim();
+    }
+
+    private void exp_rel(){
+        if (p.token==108) {
+            p=p.sig;
+        }
+        else if (p.token==109) {
+            p=p.sig;
+        }
+        else if (p.token==110) {
+            p=p.sig;
+        }
+        else if (p.token==111) {
+            p=p.sig;
+        }
+        else if (p.token==112) {
+            p=p.sig;
+        }
+        else if (p.token==113) {
+            p=p.sig;
+        }
+        else {
+            System.out.println("Se espera un operador relacional");
+        }
+    }
+    private void exp_sim() {
+        if (p.token == 100 || p.token == 116) {
+            termino();
+        } else if (p.token == 103 || p.token == 104) {
+            signo();
+            termino();
+        } else if((p.token!=108||p.token!=109||p.token!=110||p.token!=111||p.token!=112||p.token!=113)||p.token!=125){
+            
+            //Arreglar mamada
+            System.out.println(" XD "+p.token);
+            exp_sim();
+            op_aditivo();
+            termino();
+        }
+    }
+
+    private void op_aditivo() {
+        if (p.token == 103) {
+            p = p.sig;
+        } else if (p.token == 104) {
+            p = p.sig;
+        } else if (p.token == 115) {
+            p = p.sig;
+        } else {
+            System.out.println("Se espera un operador aditivo");
+        }
+    }
+
+    private void signo() {
+        if (p.token == 103) {
+            p = p.sig;
+        } else if (p.token == 104) {
+            p = p.sig;
+
+        }
+    }
+
+    private void termino() {
+        if (p.token == 100 || p.token == 116) {
+            factor();
+        } else {
+            termino();
+            op_mult();
+            factor();
+        }
+
+    }
+
+    private void factor() {
+        if (p.token == 100) {
+            p = p.sig;
+        } else if (p.token == 116) {
+            p = p.sig;
+            factor();
+        } else {
+            exp_sim();
+        }
+    }
+
+    private void op_mult() {
+        if (p.token == 105) {
+            p = p.sig;
+        } else if (p.token == 106) {
+            p = p.sig;
+        } else if (p.token == 114) {
+            p = p.sig;
+        } else {
+            System.out.println("Se espera un operador multiplicativo");
+        }
+
+    }
+    
 }
